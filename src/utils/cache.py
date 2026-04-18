e080183bb5583eaee6d48b0989a2240763166b4e
mileoa <ezkimozzzzz@gmail.com>
2026-04-18 23:14:24 +1000

    add cache realisation

--- src/utils/cache.py
+++ src/utils/cache.py
@@ -1,1 +1,13 @@
 # Кеширование
+class Cashing:
+    def __init__(self):
+        self._cache = {}
+
+    def get(self, key):
+        return self._cache[key]
+
+    def update(self, key, value):
+        self._cache[key] = value
+
+    def has(self, key):
+        return key in self._cache
