e9c8d0320d43d79ff8eea54e36a46bd5861b45d9
mileoa <ezkimozzzzz@gmail.com>
2026-04-18 23:09:57 +1000

    add comments order.py

--- src/models/order.py
+++ src/models/order.py
@@ -7,6 +7,7 @@ class Order:
     def __init__(
         self, order_id: int, total: float, user: User, products: list[Product]
     ):
+        """Создает заказ"""
         self._order_id: int = order_id
         self._total: float = total
         self._user: User = user
@@ -16,11 +17,13 @@ class Order:
         self._products: list[Product] = products
 
     def add_product(self, product: Product) -> None:
+        """Добавить продукт к заказу"""
         if not isinstance(product, Product):
             raise ValidationError("Товар должен быть объектом класса Product")
         self._products.append(product)
 
     def calculate_total(self) -> float:
+        """Получить стоимость заказа"""
         total_price: float = 0.0
         for product in self._products:
             total_price += product.get_total_price()
@@ -29,6 +32,7 @@ class Order:
         return total_price_rounded
 
     def remove_product(self, product_name: str):
+        """Удалить продукт из заказа"""
         index_to_delete: int = -1
         for product_i, product in enumerate(self._products):
             if product.name == product_name:
