f9cbaec6e33c05e226dc396b7fd4acd964dcc3fa
mileoa <ezkimozzzzz@gmail.com>
2026-04-18 23:11:42 +1000

    add comments user.py

--- src/models/user.py
+++ src/models/user.py
@@ -3,6 +3,7 @@ from models.exceptions import ValidationError
 
 class User:
     def __init__(self, name: str, email: str):
+        """Создать пользователя"""
         self._name: str = name
 
         if "@" not in email:
@@ -14,9 +15,11 @@ class User:
         return f"Пользователь: {self._name}, Email: {self._email}"
 
     def set_email(self, email: str) -> None:
+        """Изменить email"""
         if "@" not in email:
             raise ValidationError("Неверный формат email")
         self._email = email
 
     def get_email(self) -> str:
+        """Получить email"""
         return self._email
